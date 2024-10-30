785a6dad658f67bbd168a2b2fca706731c91a8ca
Kelvyh <kelvyhallyson4@gmail.com>
2024-10-29 21:09:18 -0300

    Classe Vertice separado do arquivo de testes de funcionalidades

--- questao1.js
+++ questao1.js
@@ -26,36 +26,4 @@ export default class Vertice {
     equals(vertice2) {
         return this.#x === vertice2.#x && this.#y === vertice2.#y;
     }
-}
-
-// const prompt = require('prompt-sync')();
-const readline = require('readline');
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-function askQuestion(query) {
-    return new Promise(resolve => rl.question(query, resolve));
-}
-
-async function main() {
-    const vertices = [];
-    for (let i = 0; i < 3; i++) {
-        const x = await askQuestion(`Digite o x do ${i + 1}º vértice: `);
-        const y = await askQuestion(`Digite o y do ${i + 1}º vértice: `);
-        vertices.push(new Vertice(parseFloat(x), parseFloat(y)));
-    }
-
-    console.log(`Distância entre o vértice 1 e o vértice 2: ${vertices[0].getDistancia(vertices[1])}`);
-    console.log(`Distância entre o vértice 2 e o vértice 3: ${vertices[1].getDistancia(vertices[2])}`);
-    console.log(`Distância entre o vértice 1 e o vértice 3: ${vertices[0].getDistancia(vertices[2])}`);
-
-    console.log(`Vértice 1 é igual ao vértice 2?: ${vertices[0].equals(vertices[1])}`);
-    console.log(`Vértice 2 é igual ao vértice 3?: ${vertices[1].equals(vertices[2])}`);
-    console.log(`Vértice 1 é igual ao vértice 3?: ${vertices[0].equals(vertices[2])}`);
-    rl.close();
-}
-
-main();
+};
---
+++ testeq1.js
@@ -0,0 +1,22 @@
+import Vertice from './questao1';
+import promptSync from 'prompt-sync';
+var prompt = promptSync();
+
+function main() {
+    const vertices = [];
+    for (let i = 0; i < 3; i++) {
+        const x = prompt(`Digite o x do ${i + 1}º vértice: `);
+        const y = prompt(`Digite o y do ${i + 1}º vértice: `);
+        vertices.push(new Vertice(parseFloat(x), parseFloat(y)));
+    }
+
+    console.log(`Distância entre o vértice 1 e o vértice 2: ${vertices[0].getDistancia(vertices[1])}`);
+    console.log(`Distância entre o vértice 2 e o vértice 3: ${vertices[1].getDistancia(vertices[2])}`);
+    console.log(`Distância entre o vértice 1 e o vértice 3: ${vertices[0].getDistancia(vertices[2])}`);
+
+    console.log(`Vértice 1 é igual ao vértice 2?: ${vertices[0].equals(vertices[1])}`);
+    console.log(`Vértice 2 é igual ao vértice 3?: ${vertices[1].equals(vertices[2])}`);
+    console.log(`Vértice 1 é igual ao vértice 3?: ${vertices[0].equals(vertices[2])}`);
+}
+
+main();
